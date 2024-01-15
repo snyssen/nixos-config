6075830e85b4ead19d8d9f669d6ec33fd9c45a8a
snyssen <dev@snyssen.be>
2024-01-15 22:16:51 +0100

    feat(common): add gpg

--- common/configuration.nix
+++ common/configuration.nix
@@ -102,10 +102,13 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     inputs.nix-software-center.packages."${system}".nix-software-center
+
     pkgs.gnomeExtensions.caffeine
     pkgs.gnomeExtensions.pop-shell
     pkgs.gnomeExtensions.gsconnect
     pkgs.gnomeExtensions.dash-to-dock
+
+    pkgs.gnupg
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

802b0567dfdd65b7a880e64469217dfd89c44c02
Simon Nyssen <dev@snyssen.be>
2024-07-08 07:53:58 +0200

    feat(gnome): add gnome extensions

--- nixosModules/features/gnome.nix
+++ nixosModules/features/gnome.nix
@@ -1,8 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    pkgs.gnomeExtensions.caffeine
+    pkgs.gnomeExtensions.pop-shell
+    pkgs.gnomeExtensions.gsconnect
+    pkgs.gnomeExtensions.dash-to-dock
+    pkgs.gnomeExtensions.syncthing-indicator
+  ];
 }

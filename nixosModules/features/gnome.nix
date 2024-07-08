46a305c11f2fa79e3a43b77f8fcc0927d61a393b
Simon Nyssen <dev@snyssen.be>
2024-07-08 07:55:05 +0200

    feat(gnome): add dconf settings

--- nixosModules/features/gnome.nix
+++ nixosModules/features/gnome.nix
@@ -13,4 +13,15 @@
     pkgs.gnomeExtensions.dash-to-dock
     pkgs.gnomeExtensions.syncthing-indicator
   ];
+
+  dconf = {
+    enable = true;
+    settings."org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+    settings."org/gnome/desktop/screensaver".lock-delay = lib.hm.gvariant.mkUint32 300;
+    settings."org/gnome/desktop/session".idle-delay = lib.hm.gvariant.mkUint32 600;
+    settings."org/gnome/shell/extensions/pop-shell".tile-by-default = true;
+  };
 }

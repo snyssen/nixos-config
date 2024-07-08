bf448e5f32f96804afc277cd5a89a7aa71f1194f
Simon Nyssen <dev@snyssen.be>
2024-07-08 22:14:54 +0200

    feat(hosts/gaming): [TEST] migrate to modularized config

--- hosts/gaming/configuration.nix
+++ hosts/gaming/configuration.nix
@@ -1,90 +1,63 @@
-{ inputs, outputs, lib, config, pkgs, user, secrets, hostname, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  outputs,
+  system,
+  myLib,
+  hm,
+  ...
+}:
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
-  boot.loader = {
-    timeout = null; # Wait indefinitely
-    efi.canTouchEfiVariables = true;
-    grub = {
+  myNixOS = {
+    user = {
       enable = true;
-      device = "nodev";
-      useOSProber = true;
-      efiSupport = true;
+      home-manager = {
+        enable = true;
+        config = ./home.nix;
+      };
+      zsh.enable = true;
+    };
+
+    bundles = {
+      gnome.enable = true;
+    };
+
+    grub.enable = true;
+    nvidia.enable = true;
+    syncthing = {
+      enable = true;
+      username = "snyssen";
+      devices = {
+        # TODO: export into a "data" module for easy re-use
+        "sync.snyssen.be" = {
+          id = "6QHAMO3-PARP6UW-3Y5J7I4-P5PN2C5-RHS6OMP-QCSBP5Z-6OBZMC3-NZM65QT";
+        };
+      };
+      folders = {
+        PrismLauncher = {
+          path = "/home/snyssen/.local/share/PrismLauncher";
+          devices = [ "sync.snyssen.be" ];
+        };
+        # RetroArch = {
+        #   path = "/home/snyssen/.config/retroarch";
+        #   devices = [ "sync.snyssen.be" ];
+        # };
+      };
     };
   };
 
   # Fix for time changing between boot of Windows and Linux
   time.hardwareClockInLocalTime = true;
 
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-  programs.xwayland.enable = true;
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
-
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "fr";
-    xkbVariant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "fr";
-
-  services.syncthing = {
-    enable = true;
-    user = "${user}";
-    dataDir = "/home/${user}/Documents";
-    configDir = "/home/${user}/Documents/.config/syncthing";
-    overrideDevices = true;
-    overrideFolders = true;
-    devices = secrets.syncthing.devices;
-    folders = secrets.syncthing.folders;
-  };
-
-  networking = {
-    # Open ports in the firewall for:
-    # - syncthing
-    firewall.allowedTCPPorts = [ 22000 ];
-    firewall.allowedUDPPorts = [ 22000 21027 ];
-  };
-
   programs.gamemode.enable = true;
+  
+  system.name = "gaming";
+  system.stateVersion = "23.05";
 }
--- hosts/gaming/home.nix
+++ hosts/gaming/home.nix
@@ -1,11 +1,23 @@
 { inputs, lib, config, pkgs, user, hostname, ... }: {
-    home.packages = with pkgs; [
-        prismlauncher
-        gnome.dconf-editor
-        (retroarch.override {
-            cores = with libretro; [
-                pcsx2
-            ];
-        })
-    ];
+  myHomeManager = {
+    bundles = {
+      dev.enable = true;
+    };
+
+    dconf.enable = true;
+  };
+
+  # TODO: move
+  home.packages = with pkgs; [
+      prismlauncher
+      gnome.dconf-editor
+      (retroarch.override {
+          cores = with libretro; [
+              pcsx2
+          ];
+      })
+  ];
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
 }

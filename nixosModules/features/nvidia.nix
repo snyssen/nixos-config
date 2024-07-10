43a169c78719c21b5672d78dbabbd0edecdfdb35
Simon Nyssen <dev@snyssen.be>
2024-07-10 18:31:53 +0200

    fix(nixos/nvidia): disable open source drivers

--- nixosModules/features/nvidia.nix
+++ nixosModules/features/nvidia.nix
@@ -16,7 +16,11 @@
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
@@ -28,7 +32,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
 	  # accessible via `nvidia-settings`.

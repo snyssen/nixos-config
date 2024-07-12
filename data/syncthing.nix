7ba2a6842fffe8cf5c3d10d144891bc76612353f
Simon Nyssen <dev@snyssen.be>
2024-07-12 17:16:28 +0200

    feat(data/syncthing): add workstation id

--- data/syncthing.nix
+++ data/syncthing.nix
@@ -3,5 +3,8 @@
     "sync.snyssen.be" = {
       id = "6QHAMO3-PARP6UW-3Y5J7I4-P5PN2C5-RHS6OMP-QCSBP5Z-6OBZMC3-NZM65QT";
     };
+    "fedora-workstation" = {
+      id = "R6CGGBH-U7FT5Z4-S5VAMC3-BPYZHTX-67F7ENV-FB234XB-MDH3HRW-EOJU2QP";
+    };
   };
 }

fba2494954ae8ab2ba9bbcaf6efe9b40cc1d869e
snyssen <dev@snyssen.be>
2024-09-01 09:48:46 +0200

    fix(home/vscode): install C# extension the nix way to see if it resolves issue with C# debugger

--- homeManagerModules/features/vscode.nix
+++ homeManagerModules/features/vscode.nix
@@ -2,42 +2,44 @@
   programs.vscode = {
     enable = true;
     # TODO: find a way to propagate system architecture from flake.nix
-    extensions = with inputs.nix-vscode-extensions.extensions."x86_64-linux"; [
+    extensions = with inputs.nix-vscode-extensions.extensions."x86_64-linux".vscode-marketplace; [
       # TODO: only add extensions that are actually needed for all projects
       # I would like to move onto devcontainers for all projects, with extensions specific for each project defined in the project
-      vscode-marketplace.redhat.ansible
-      vscode-marketplace.astro-build.astro-vscode
-      vscode-marketplace.aaron-bond.better-comments
-      vscode-marketplace.streetsidesoftware.code-spell-checker
-      vscode-marketplace.ms-vscode-remote.remote-containers
-      vscode-marketplace.ms-azuretools.vscode-docker
-      vscode-marketplace.hediet.vscode-drawio
-      vscode-marketplace.editorconfig.editorconfig
-      vscode-marketplace.dbaeumer.vscode-eslint
-      vscode-marketplace.github.vscode-github-actions
-      vscode-marketplace.eamodio.gitlens
-      vscode-marketplace.golang.go
-      vscode-marketplace.hashicorp.terraform
-      vscode-marketplace.oderwat.indent-rainbow
-      vscode-marketplace.whtouche.vscode-js-console-utils
-      vscode-marketplace.yzhang.markdown-all-in-one
-      vscode-marketplace.davidanson.vscode-markdownlint
-      vscode-marketplace.unifiedjs.vscode-mdx
-      vscode-marketplace.bbenoist.nix
-      vscode-marketplace.christian-kohler.npm-intellisense
-      vscode-marketplace.yutengjing.open-in-external-app
-      vscode-marketplace.johnpapa.vscode-peacock
-      vscode-marketplace.esbenp.prettier-vscode
-      vscode-marketplace.ms-python.python
-      vscode-marketplace.redhat.vscode-commons
-      vscode-marketplace.ms-vscode-remote.vscode-remote-extensionpack
-      vscode-marketplace.timonwong.shellcheck
-      vscode-marketplace.bradlc.vscode-tailwindcss
-      vscode-marketplace.gruntfuggly.todo-tree
-      vscode-marketplace.bbenoist.vagrant
-      vscode-marketplace.vscode-icons-team.vscode-icons
-      vscode-marketplace.ms-vscode-remote.remote-wsl
-      vscode-marketplace.redhat.vscode-yaml
+      redhat.ansible
+      astro-build.astro-vscode
+      aaron-bond.better-comments
+      streetsidesoftware.code-spell-checker
+      ms-vscode-remote.remote-containers
+      ms-azuretools.vscode-docker
+      hediet.vscode-drawio
+      editorconfig.editorconfig
+      dbaeumer.vscode-eslint
+      github.vscode-github-actions
+      eamodio.gitlens
+      golang.go
+      hashicorp.terraform
+      oderwat.indent-rainbow
+      whtouche.vscode-js-console-utils
+      yzhang.markdown-all-in-one
+      davidanson.vscode-markdownlint
+      unifiedjs.vscode-mdx
+      bbenoist.nix
+      christian-kohler.npm-intellisense
+      yutengjing.open-in-external-app
+      johnpapa.vscode-peacock
+      esbenp.prettier-vscode
+      ms-python.python
+      redhat.vscode-commons
+      ms-vscode-remote.vscode-remote-extensionpack
+      timonwong.shellcheck
+      bradlc.vscode-tailwindcss
+      gruntfuggly.todo-tree
+      bbenoist.vagrant
+      vscode-icons-team.vscode-icons
+      ms-vscode-remote.remote-wsl
+      redhat.vscode-yaml
+      ms-dotnettools.csharp
+      ms-dotnettools.csdevkit
     ];
   };
 }

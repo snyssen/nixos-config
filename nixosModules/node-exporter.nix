27c978bfd865dae102fde220685204668380e31c
snyssen <dev@snyssen.be>
2024-12-16 08:44:42 +0100

    feat(nixosModules/node-exporter): add Prometheus node-exporter feature

---
+++ nixosModules/node-exporter.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+let
+  cfg = config.myNixOS.node-exporter;
+in
+{
+  options.myNixOS.node-exporter = {
+    port = lib.mkOption {
+      default = 9000;
+      description = "Port under which node exporter metrics are available";
+    };
+  };
+
+  # https://nixos.org/manual/nixos/stable/#module-services-prometheus-exporters
+  services.prometheus.exporters.node = {
+    enable = true;
+    port = cfg.port;
+    # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/exporters.nix
+    # enabledCollectors = [ "systemd" ];
+    # /nix/store/zgsw0yx18v10xa58psanfabmg95nl2bb-node_exporter-1.8.1/bin/node_exporter  --help
+    # extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" "--collector.wifi" ];
+  };
+}

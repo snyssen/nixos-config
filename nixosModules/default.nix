df1f089a351381f034f24ff04d2758fd7d3b81e7
snyssen <dev@snyssen.be>
2025-05-09 15:23:18 +0200

    feat(home/vscode): reduce default extensions to the minimum

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -1,45 +1,29 @@
-{ pkgs
-, config
-, lib
-, inputs
-, outputs
-, myLib
-, ...
-}:
+{ pkgs, config, lib, inputs, outputs, myLib, ... }:
 let
   cfg = config.myNixOS;
 
   # Taking all modules in ./features and adding enables to them
-  features =
-    myLib.extendModules
-      (name: {
-        extraOptions = {
-          myNixOS.${name}.enable = lib.mkEnableOption "enable my ${name} configuration";
-        };
+  features = myLib.extendModules (name: {
+    extraOptions = {
+      myNixOS.${name}.enable =
+        lib.mkEnableOption "enable my ${name} configuration";
+    };
 
-        configExtension = config: (lib.mkIf cfg.${name}.enable config);
-      })
-      (myLib.filesIn ./features);
+    configExtension = config: (lib.mkIf cfg.${name}.enable config);
+  }) (myLib.filesIn ./features);
 
   # Taking all module bundles in ./bundles and adding bundle.enables to them
-  bundles =
-    myLib.extendModules
-      (name: {
-        extraOptions = {
-          myNixOS.bundles.${name}.enable = lib.mkEnableOption "enable ${name} module bundle";
-        };
+  bundles = myLib.extendModules (name: {
+    extraOptions = {
+      myNixOS.bundles.${name}.enable =
+        lib.mkEnableOption "enable ${name} module bundle";
+    };
 
-        configExtension = config: (lib.mkIf cfg.bundles.${name}.enable config);
-      })
-      (myLib.filesIn ./bundles);
+    configExtension = config: (lib.mkIf cfg.bundles.${name}.enable config);
+  }) (myLib.filesIn ./bundles);
 
-in
-{
-  imports =
-    [
-      inputs.home-manager.nixosModules.home-manager
-    ]
-    ++ features
+in {
+  imports = [ inputs.home-manager.nixosModules.home-manager ] ++ features
     ++ bundles;
 
   config = {
@@ -53,5 +37,6 @@ in
       options = "--delete-older-than 7d";
     };
     nixpkgs.config.allowUnfree = true;
+    nixpkgs.overlays = [ inputs.nix-vscode-extensions.overlays.default ];
   };
 }

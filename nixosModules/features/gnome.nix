6760a7da5be8ee30d40ab03b726c8fa8ee008bf0
snyssen <dev@snyssen.be>
2025-07-24 18:12:45 +0200

    chore(modules/gnome): disable xserver since it is not actually in use

--- nixosModules/features/gnome.nix
+++ nixosModules/features/gnome.nix
@@ -8,9 +8,6 @@ in {
     };
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;

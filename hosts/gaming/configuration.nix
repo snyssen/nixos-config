e15b2857b3ff5ad1d586ba749021fe205385f877
snyssen <dev@snyssen.be>
2025-08-02 20:05:51 +0200

    feat(hosts/gaming): add tailscale

--- hosts/gaming/configuration.nix
+++ hosts/gaming/configuration.nix
@@ -79,6 +79,7 @@ in
     logitech.enable = true;
     node-exporter.enable = true;
     docker.enable = true;
+    tailscale.enable = true;
   };
 
   # Fix for time changing between boot of Windows and Linux

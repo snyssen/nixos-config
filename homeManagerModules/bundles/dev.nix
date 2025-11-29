491c53b352529376d0b4a83f213fcafa72207f0f
snyssen <dev@snyssen.be>
2025-11-29 10:42:13 +0100

    feat(home/dev): enable intelli-shell per default

--- homeManagerModules/bundles/dev.nix
+++ homeManagerModules/bundles/dev.nix
@@ -1,9 +1,10 @@
-{ lib, ... }: {
+{ lib, ... }:
+{
   myHomeManager = {
     zsh.enable = true;
     zsh.atuin.enable = lib.mkDefault false;
     zsh.fzf.enable = lib.mkDefault true;
-    zsh.intelli-shell.enable = lib.mkDefault false;
+    zsh.intelli-shell.enable = lib.mkDefault true;
     git.enable = true;
     vscode.enable = true;
     direnv.enable = true;
